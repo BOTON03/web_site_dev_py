2ffe6c9ab0a8ecd7517349133387bdd9db1f44d8
sebastian_dev <jruedamonsalve3@gmail.com>
2025-05-12 08:04:18 -0500

    v4

--- test_auth.py
+++
@@ -1,8 +0,0 @@
-# tests/test_auth.py
-import unittest
-from zoho_sync.zoho.auth import get_access_token
-
-class TestAuth(unittest.TestCase):
-    def test_get_access_token(self):
-        token = get_access_token()
-        self.assertIsNotNone(token)
--- test_sync.py
+++
@@ -1,5 +0,0 @@
-from .test_connections import test_connections
-
-if __name__ == '__main__':
-    test_connections()
-
